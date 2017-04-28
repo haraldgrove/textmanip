95d252d0110df491beb213a411e05e2c0db0229b
Harald Grove <haraldgrove@gmail.com>
2017-04-28 09:38:29 +0700

    Update to count.py

--- count.py
+++ count.py
@@ -1,45 +1,47 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # Author: Harald Grove
-# Counts occurrences of each record in a given column
+# Counts occurrences of each record in a given set of columns
 
 import sys
-import time
-import os
-from optparse import OptionParser
+import argparse
 
 def countFile(opt):
-    """ Removes lines not matching given criteria
     """
-    col = int(opt.column) - 1
-    fin = open(opt.infile,'r')
-    hist = {}
-    for line in fin:
-        if line.startswith('#'):
-            continue
-        l = line.strip().split('\t')
-        hist[l[col]] = hist.get(l[col],0) + 1
-    fin.close()
-    fout = open(opt.outfile,'w')
-    for key in hist:
-        fout.write('%s\t%s\n' % (key,hist[key]))
+    Count number of occurrences
+    """
+    if ',' in opt.column:
+        cols = [int(c)-1 for c in opt.column.split(',')]
+    else:
+        cols = [int(opt.column) - 1]
+    with open(opt.infile,'r') as fin:
+        hist = {}
+        for line in fin:
+            if line.startswith('#'):
+                continue
+            line_l = line.strip().split('\t')
+            record = []
+            for c in cols:
+                record.append(line_l[c])
+            record = tuple(record)
+            hist[record] = hist.get(record,0) + 1
+    if opt.outfile is not None:
+        fout = open(opt.outfile,'w')
+    else:
+        fout = sys.stdout
+    for key, value in hist.items():
+        k = '\t'.join(key)
+        fout.write('{}\t{}\n'.format(k,value))
     fout.close()
 
 def main():
-    usage = "usage: %prog  [options]"
-    parser = OptionParser(usage)
-    parser.add_option("-i",dest="infile",help="input file", metavar="FILE")
-    parser.add_option("-o",dest="outfile",help="output file", metavar="FILE")
-    parser.add_option("-c",dest="column",help="column selection", default=1)
-    parser.add_option("-v",dest="verbose",action="store_true",help="Prints runtime info",default=False)
-    parser.add_option("-G",dest="galaxy",action="store_true",help="Script is being run from galaxy",default=False)
-    (options,args) = parser.parse_args()
-    if options.galaxy:
-        if options.infile == 'None': options.infile = None
-        if options.outfile == 'None': options.outfile = None
-    countFile(options)
+    parser = argparse.ArgumentParser(description='Counts occurrences')
+    parser.add_argument("infile",help="input file")
+    parser.add_argument("-o",dest="outfile",help="output file")
+    parser.add_argument("-c",dest="column",help="column selection", default='1')
+    parser.add_argument("-v",dest="verbose",action="store_true",help="Prints runtime info",default=False)
+    args = parser.parse_args()
+    countFile(args)
 
 if __name__ == "__main__":
-    #t = time.time()
     main()
-    #print "Time spent: %.3f" % ( time.time()-t )
